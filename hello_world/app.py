4e4e168b8c97ed95320b0c18fba092f48aa25274
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2022-12-11 22:35:59 +0900

    add policy and fix app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,71 @@
 import json
+import boto3
+from retrying import retry
+from hashlib import sha256
 
 # import requests
 
+endpoint = "https://a15ks10d68w3hk-ats.iot.us-east-1.amazonaws.com"
+
+
+def get_s3_object(bucket, key):
+    s3 = boto3.client("s3")
+    m = sha256()
+
+    with open("/tmp/s3obj", "wb") as f:
+        s3.download_file(bucket, key, "/tmp/s3obj")
+
+    with open("/tmp/s3obj", "rb") as f:
+        while True:
+            chunk = f.read(1024 * 1024)
+            if len(chunk) == 0:
+                break
+            m.update(chunk)
+        return m.hexdigest()
+
+
+@retry(stop_max_attempt_number=2, wait_fixed=1000)
+def _thing_shadow(iot_data, thing_name):
+    try:
+        return iot_data.get_thing_shadow(thingName=thing_name)
+    except iot_data.exceptions.ResourceNotFoundException:
+        return None
+
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    iot_data = boto3.client("iot-data", endpoint_url=endpoint)
+    iot = boto3.client("iot")
+    things = []
+    next_token = ""
+    while True:
+        # list_things = iot.list_things(nextToken=next_token, maxResults=1) # for debug
+        list_things = iot.list_things(nextToken=next_token)
+        things.extend(list_things["things"])
+        next_token = list_things.get("nextToken")
+        if not next_token:
+            break
+    print(things)
+    for thing in things:
+        shadow = _thing_shadow(iot_data, thing["thingName"])
+        if shadow:
+            payload = shadow["payload"].read().decode("utf-8")
+            print(f'{thing["thingName"]}, {json.loads(payload)}')
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
+    digest = get_s3_object(bucket, key)
+    print(digest)
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        "body": json.dumps(
+            {
+                "message": "hello world",
+                # "location": ip.text.replace("\n", "")
+            }
+        ),
     }
+
+
+if __name__ == "__main__":
+    print(lambda_handler({}, {}))
